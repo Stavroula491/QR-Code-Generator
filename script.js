381e1588f76dc5ff04e0475040778a4eebde9611
Stavroula491 <65353583+Stavroula491@users.noreply.github.com>
2025-02-03 11:48:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function reGeneratorEnabled(){
+    document.querySelector(".re-generator").disabled = false;
+    document.querySelector(".generator").disabled = true;
+};
+
+function reGeneratorDisabled(){
+    document.querySelector(".re-generator").disabled = true;
+    document.querySelector(".generator").disabled = false;
+};
+
+
+
 document.querySelector(".generator").addEventListener("click", function(){
     const text = document.getElementById("URL").value;
     const qrImage = document.getElementById('qrcode');
@@ -13,5 +25,14 @@ document.querySelector(".generator").addEventListener("click", function(){
                 qrImage.src = url;
             }
         });
-
+    
+    reGeneratorEnabled();
     });
+
+document.querySelector(".re-generator").addEventListener("click", function(){
+    reGeneratorDisabled();
+
+    document.getElementById('qrcode').src = "./blurred_qr_code.png";
+    document.getElementById("URL").value = "";
+
+});
